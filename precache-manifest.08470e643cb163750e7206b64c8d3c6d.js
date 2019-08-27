95cdfe7e2232a7b130c779533166b5a00dc08716
unknown <jgyanez1@gmail.com>
2019-08-27 16:51:21 -0400

    Updates

---
+++ precache-manifest.08470e643cb163750e7206b64c8d3c6d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15aaf09df412ee9c2cd139defaa7292b",
+    "url": "/Sipa/index.html"
+  },
+  {
+    "revision": "47284ccd9a907cce3593",
+    "url": "/Sipa/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "ca2b60421cd294935756",
+    "url": "/Sipa/static/css/main.aadd5ce6.chunk.css"
+  },
+  {
+    "revision": "47284ccd9a907cce3593",
+    "url": "/Sipa/static/js/2.5b9a579d.chunk.js"
+  },
+  {
+    "revision": "ca2b60421cd294935756",
+    "url": "/Sipa/static/js/main.9e8332a2.chunk.js"
+  },
+  {
+    "revision": "b241477417ad8fb4e3cb",
+    "url": "/Sipa/static/js/runtime~main.26ae4982.js"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/Sipa/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/Sipa/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/Sipa/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/Sipa/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/Sipa/static/media/sidebar-4.fc9cb053.jpg"
+  }
+]);
