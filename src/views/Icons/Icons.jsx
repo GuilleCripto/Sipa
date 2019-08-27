29d2be045235ded19e7953f8282bd72f26621635
unknown <jgyanez1@gmail.com>
2019-08-27 16:39:54 -0400

    buid

---
+++ src/views/Icons/Icons.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import	 ProductBox from './index';
+export default class Icons extends Component {
+  render() {
+    return (
+     <ProductBox/>
+    );
+  }
+
+}
---
+++ src/views/Typography/Typography.jsx
@@ -0,0 +1,162 @@
+/*!
+
+=========================================================
+* Material Dashboard React - v1.7.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard-react
+* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/material-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+// nodejs library to set properties for components
+import PropTypes from "prop-types";
+// @material-ui/core components
+import withStyles from "@material-ui/core/styles/withStyles";
+// core components
+import Quote from "components/Typography/Quote.jsx";
+import Muted from "components/Typography/Muted.jsx";
+import Primary from "components/Typography/Primary.jsx";
+import Info from "components/Typography/Info.jsx";
+import Success from "components/Typography/Success.jsx";
+import Warning from "components/Typography/Warning.jsx";
+import Danger from "components/Typography/Danger.jsx";
+import Card from "components/Card/Card.jsx";
+import CardHeader from "components/Card/CardHeader.jsx";
+import CardBody from "components/Card/CardBody.jsx";
+
+const style = {
+  typo: {
+    paddingLeft: "25%",
+    marginBottom: "40px",
+    position: "relative"
+  },
+  note: {
+    fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+    bottom: "10px",
+    color: "#c0c1c2",
+    display: "block",
+    fontWeight: "400",
+    fontSize: "13px",
+    lineHeight: "13px",
+    left: "0",
+    marginLeft: "20px",
+    position: "absolute",
+    width: "260px"
+  },
+  cardCategoryWhite: {
+    color: "rgba(255,255,255,.62)",
+    margin: "0",
+    fontSize: "14px",
+    marginTop: "0",
+    marginBottom: "0"
+  },
+  cardTitleWhite: {
+    color: "#FFFFFF",
+    marginTop: "0px",
+    minHeight: "auto",
+    fontWeight: "300",
+    fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+    marginBottom: "3px",
+    textDecoration: "none"
+  }
+};
+function TypographyPage(props) {
+  const { classes } = props;
+  return (
+    <Card>
+      <CardHeader color="primary">
+        <h4 className={classes.cardTitleWhite}>Facturas y Pagos</h4>
+        <p className={classes.cardCategoryWhite}>
+          Facturas y Pagos
+        </p>
+      </CardHeader>
+      <CardBody>
+      
+       
+        <div className={classes.typo}>
+          <div className={classes.note}>Paragraph</div>
+          <p>
+            I will be the leader of a company that ends up being worth billions
+            of dollars, because I got the answers. I understand culture. I am
+            the nucleus. I think that’s a responsibility that I have, to push
+            possibilities, to show people, this is the level that things could
+            be at.
+          </p>
+        </div>
+        <div className={classes.typo}>
+          <div className={classes.note}>Quote</div>
+          <Quote
+            text="I will be the leader of a company that ends up being worth billions of dollars, because I got the answers. I understand culture. I am the nucleus. I think that’s a responsibility that I have, to push possibilities, to show people, this is the level that things could be at."
+            author=" Kanye West, Musician"
+          />
+        </div>
+        <div className={classes.typo}>
+          <div className={classes.note}>Muted Text</div>
+          <Muted>
+            I will be the leader of a company that ends up being worth billions
+            of dollars, because I got the answers...
+          </Muted>
+        </div>
+        <div className={classes.typo}>
+          <div className={classes.note}>Primary Text</div>
+          <Primary>
+            I will be the leader of a company that ends up being worth billions
+            of dollars, because I got the answers...
+          </Primary>
+        </div>
+        <div className={classes.typo}>
+          <div className={classes.note}>Info Text</div>
+          <Info>
+            I will be the leader of a company that ends up being worth billions
+            of dollars, because I got the answers...
+          </Info>
+        </div>
+        <div className={classes.typo}>
+          <div className={classes.note}>Success Text</div>
+          <Success>
+            I will be the leader of a company that ends up being worth billions
+            of dollars, because I got the answers...
+          </Success>
+        </div>
+        <div className={classes.typo}>
+          <div className={classes.note}>Warning Text</div>
+          <Warning>
+            I will be the leader of a company that ends up being worth billions
+            of dollars, because I got the answers...
+          </Warning>
+        </div>
+        <div className={classes.typo}>
+          <div className={classes.note}>Danger Text</div>
+          <Danger>
+            I will be the leader of a company that ends up being worth billions
+            of dollars, because I got the answers...
+          </Danger>
+        </div>
+        <div className={classes.typo}>
+          <div className={classes.note}>Small Tag</div>
+          <h2>
+            Header with small subtitle
+            <br />
+            <small>
+              Use {'"'}Small{'"'} tag for the headers
+            </small>
+          </h2>
+        </div>
+      </CardBody>
+    </Card>
+  );
+}
+
+TypographyPage.propTypes = {
+  classes: PropTypes.object
+};
+
+export default withStyles(style)(TypographyPage);
